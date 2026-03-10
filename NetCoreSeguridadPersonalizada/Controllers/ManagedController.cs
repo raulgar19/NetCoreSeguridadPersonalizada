7a435957ecc1689e9663e8b39ec3917a054534c1
raulgar19 <raulgamu2003@gmail.com>
2026-03-10 11:46:29 +0100

    Add project files.

---
+++ NetCoreSeguridadPersonalizada/Controllers/ManagedController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace NetCoreSeguridadPersonalizada.Controllers
+{
+    public class ManagedController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            if (username.ToLower() == "admin" && password == "12345")
+            {
+                ClaimsIdentity identity = new ClaimsIdentity(
+                    CookieAuthenticationDefaults.AuthenticationScheme, ClaimTypes.Name, ClaimTypes.Role);
+
+                Claim claimUserName = new Claim(ClaimTypes.Name, username);
+                Claim claimRole = new Claim(ClaimTypes.Role, "USUARIO");
+
+                identity.AddClaim(claimUserName);
+                identity.AddClaim(claimRole);
+
+                ClaimsPrincipal userPrincipal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    userPrincipal,
+                    new AuthenticationProperties
+                    {
+                        ExpiresUtc = DateTime.Now.AddMinutes(10)
+                    });
+
+                return RedirectToAction("Perfil", "Usuarios");
+            }
+            else
+            {
+                ViewData["MENSAJE"] = "Credenciales incorrectas";
+                return View();
+            }
+        }
+    }
+}
---
+++ NetCoreSeguridadPersonalizada/Filters/AuthorizeUsersAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace NetCoreSeguridadPersonalizada.Filters
+{
+    public class AuthorizeUsersAttribute : AuthorizeAttribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var user = context.HttpContext.User;
+
+            if (user.Identity.IsAuthenticated == false)
+            {
+                RouteValueDictionary rutaLogin = new RouteValueDictionary(new { controller = "Managed", action = "Login" });
+                context.Result = new RedirectToRouteResult(rutaLogin);
+            }
+        }
+    }
+}
